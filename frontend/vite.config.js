e81140521dc8898d8b14eb42cb9778766fa020fe
MAHRifat <hasanrifata2@gmail.com>
2025-05-01 07:29:19 +0600

    config vite for deployment

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: '../build', // Outputs to root/build directory
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          react: ['react', 'react-dom'],
+          mui: ['@mui/material', '@mui/icons-material'],
+          vendor: ['axios', 'moment', 'socket.io-client']
+        }
+      }
+    },
+    chunkSizeWarningLimit: 1000
+  }
 })
