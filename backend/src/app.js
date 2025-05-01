e2accf7861f8748c52fba66d193d1ec15f602b26
MAHRifat <hasanrifata2@gmail.com>
2025-05-01 09:21:51 +0600

    update cors

--- backend/src/app.js
+++ backend/src/app.js
@@ -19,13 +19,30 @@ const io = connectToSocket(server)
 
 
 app.set("port", (process.env.PORT || 3000))
-app.use(cors({
-    origin: 'https://unitytalk-60xq.onrender.com/',
+const allowedOrigins = [
+    'http://localhost:5173',
+    'https://unitytalk-60xq.onrender.com',
+    'https://unitytalk-3ubl.onrender.com/',
+    'https://unitytalk-a-video-meeting-site.onrender.com/'
+    // Add other environments as needed
+  ];
+  
+  app.use(cors({
+    origin: function (origin, callback) {
+      // Allow requests with no origin (like mobile apps or curl requests)
+      if (!origin) return callback(null, true);
+      
+      if (allowedOrigins.indexOf(origin) !== -1) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
-    exposedHeaders: ['Authorization'] // Add this line
-}));
+    exposedHeaders: ['Authorization']
+  }));
 app.use(express.json({ limit: "40kb" }))
 app.use(express.urlencoded({ limit: "40kb", extended: true }))
 
